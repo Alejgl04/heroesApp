791cf9b2d638bfeef46468ce402ec8455b3b0fa9
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-07-23 19:25:41 -0400

    hero by id

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { enviroments } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -11,8 +11,14 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
-
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  getHeroById( id: string ): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${id}`)
+      .pipe(
+        catchError( error => of(undefined))
+      )
   }
 }
